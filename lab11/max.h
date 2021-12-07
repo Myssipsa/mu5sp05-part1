a4e99c9532b038ddf5960cea0d6c99d830dd55a7
[myssipsa] <[myssipsa.mehraz@etu.sorbonne-universite.fr]>
2021-12-07 11:52:31 -0100

    lab11_fini

---
+++ lab11/max.h
@@ -0,0 +1,9 @@
+#ifndef H_MAX_H
+#define H_MAX_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void maximum (int tab[], int n, int *max);
+#endif
---
+++ lab11/var.c
@@ -0,0 +1,32 @@
+#include "var.h"
+
+void variance (int tab[], int n, float *var)
+{
+    int i=0;
+    float varObt=0;
+    int M=0;
+    int val=0;
+    int somme=0;
+
+    /*Calcul de la moyenne*/ 
+    for (i=0 ; i<n ; i++)
+    {
+        val=val+tab[i];
+        
+    }
+
+    M=val/n;
+
+    for (i=0 ; i<n ; i++)
+    {
+        somme=somme+(tab[i]-M)*(tab[i]-M);
+
+    }
+
+    
+    varObt=somme/n;
+
+    *var=varObt;
+
+
+}
